3b1d31089c6352f7e23ad565d8a309fd5d8c743e
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-21 17:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let element=document.getElementByid("#level")
+let level=0;
+if(element){
+	level++;
+	element= element.parentElement;
+}else if(element.nodeName=="html"){
+	return
+}
+//const parentName=document.querySelector("#level").parentElement.nodeName;
+
+ alert(`The level of the element is:${level} `);
